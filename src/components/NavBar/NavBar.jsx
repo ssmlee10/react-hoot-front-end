97da020ac58f7e3c4f4713ec1f2a98a655bc06d6
Sharon Lee <ssmlee10@gmail.com>
2025-08-20 10:07:54 -0700

    hoots index component: COMPLETE

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -15,8 +15,8 @@ const NavBar = () => {
     <nav>
       {user ? (
         <ul>
-          <li>Welcome, {user.username}</li>
-          <li><Link to='/'>Dashboard</Link></li>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/hoots'>Hoots</Link></li>
           <li><Link to='/' onClick={handleSignOut}>Sign Out</Link></li>
         </ul>
       ) : (
